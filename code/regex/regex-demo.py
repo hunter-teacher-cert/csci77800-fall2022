69af01d3373f7167e1d894a7c4ff8691e895bfa6
Mike Zamansky <zamansky@gmail.com>
2022-09-22 18:13:26 -0400

    added file access

--- code/regex/regex-demo.py
+++ code/regex/regex-demo.py
@@ -5,12 +5,14 @@ def find_date(line):
     pattern = r"\d{1,2}/\d{1,2}/\d{2,4}"
     result = re.findall(pattern,line)
 
+    pattern=r'(October|Oct|November|Nov)( [0-9]{1,2}, [0-9]{4})'
+    result = result + re.findall(pattern,line)
     return result
 
 
 f = open("datefile.dat")
 for line in f.readlines():
-    print(line)
-    #result = find_date(line)
-    #if (len(result)>0):
-    #    print(result)
+    #print(line)
+    result = find_date(line)
+    if (len(result)>0):
+        print(result)
