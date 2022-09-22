73d64eafc8f8e01829357d2e9f86de893bc1c1a7
Mike Zamansky <zamansky@gmail.com>
2022-09-22 18:05:21 -0400

    added file access

---
+++ code/regex/regex-demo.py
@@ -0,0 +1,16 @@
+import re
+
+
+def find_date(line):
+    pattern = r"\d{1,2}/\d{1,2}/\d{2,4}"
+    result = re.findall(pattern,line)
+
+    return result
+
+
+f = open("datefile.dat")
+for line in f.readlines():
+    print(line)
+    #result = find_date(line)
+    #if (len(result)>0):
+    #    print(result)
